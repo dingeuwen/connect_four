4dd345914678f7bfc3ccb8157f917d8d2b1e7fdf
Eu-wen Ding <dingeuwen@Eu-wens-MacBook-Pro.local>
2013-04-21 17:57:36 -0700

    there may be a terrible bug in hereeeee.....

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -65,7 +65,7 @@ class GamesController < ApplicationController
 
     respond_to do |format|
       if @game.save
-        format.html { redirect_to @game, notice: 'Game was successfully created.' }
+        format.html { redirect_to @game}
         format.json { render json: @game, status: :created, location: @game }
       else
         format.html { render action: "new" }
@@ -104,7 +104,7 @@ class GamesController < ApplicationController
 
     respond_to do |format|
       if @game.update_attributes(params[:game])
-        format.html { redirect_to @game, notice: 'Move made' }
+        format.html { redirect_to @game}
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
